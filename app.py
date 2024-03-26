df037b4dc4d6581d4143848440c25e989468645d
Alexander Leino <Alexander.Leino@yahoo.com>
2024-03-26 15:23:16 -0400

    installed flask_cors to allow access in next app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os 
+import os
+from flask_cors import CORS
 
 # Init App
 app = Flask(__name__)
+cors = CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
