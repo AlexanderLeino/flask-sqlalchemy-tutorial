169886380f33570b75979009994e669cfe49c830
Alexander Leino <Alexander.Leino@yahoo.com>
2024-03-26 21:13:23 -0400

    added beautiful soup package

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 from flask_cors import CORS
-
+from bs4 import BeautifulSoup
+import requests
 # Init App
 app = Flask(__name__)
 cors = CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+fetched_URL = requests.get("https://beautiful-soup-4.readthedocs.io/en/latest/").content
+
+
+soup = BeautifulSoup(fetched_URL, features='html.parser')
+print(soup)
 # Database
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
